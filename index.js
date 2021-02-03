4510aad40e1f51fcdc1233b8a2240fa221fe699d
naresh-ns <59919934+naresh-ns@users.noreply.github.com>
2021-02-03 12:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Naresh!'),
+        body: JSON.stringify('Hello from Naresh NS>>!'),
     };
     return response;
 };
